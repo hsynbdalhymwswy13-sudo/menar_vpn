ae15e4a16e3f2e734d10523716915fb2c98d55b9
hsynbdalhymwswy13-sudo <hsynbdalhymwswy13@gmail.com>
2026-09-26 00:40:46 +0330

    Menar VPN initial version

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven {
+            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+            mavenContent { snapshotsOnly() }
+            content { includeGroup("io.github.sagernet") }
+        }
+        maven { url = uri("https://api.xposed.info/") }
+    }
+}
+rootProject.name = "sing-box"
+include(":app")
+include(":libxposed-api")
+project(":libxposed-api").projectDir = file("third_party/libxposed-api")
